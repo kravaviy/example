9339c5a873192423bb3afc27bc7c8a9a6b970017
igor-krava <sasha_721a@mail.ru>
2018-01-15 01:17:54 +0300

    not work timer

---
+++ assets/js/custom.js
@@ -0,0 +1,36 @@
+/*
+  Author: bonefishcode
+  Template: Bone-S
+  Version: 1.0.1
+  URL: http://themeforest.net/user/bonefishcode
+*/
+
+/*
+place custom function at this js file
+*/
+
+(function($) {
+  'use strict';
+
+/*=================================================
+window on load
+=================================================*/
+  $(window).on('load', function() {
+    // on load function
+  });
+
+/*=================================================
+document on ready
+=================================================*/
+  $(document).on('ready', function() {
+    // on ready function
+  });
+
+/*=================================================
+window on resize
+=================================================*/
+  $(window).on('resize', function() {
+    // on resize function
+  }).trigger('resize');
+
+})(jQuery);
